15da019090614d66e6d5a309ed60e83138904bbd
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-02-13 23:11:33 +0300

    Add files via upload

--- src/components/GalleryItems/GalleryItems.tsx
+++ src/components/GalleryItems/GalleryItems.tsx
@@ -10,34 +10,22 @@ interface IGalleryItems {
 
 export const GalleryItems: React.FC<IGalleryItems> = ({ pictures, onclickPic }) => {
   return (
-    // <Box
-    //   display='flex'
-    //   justifyContent='center'
-    //   alignItems='center'
-    //   textAlign='center'
-    //   margin='0 auto'
-    //   flexWrap='wrap'
-    // >
-    //   {pictures.map((pictures) => {
-    //     return <GalleryItem onclickPic={onclickPic} key={pictures.id} picture={pictures} />;
-    //   })}
-    // </Box>
     <Box display='flex' flexDirection='row'>
-      <Box display='flex' flexDirection='column'>
+      <Box display='flex' flexDirection='column' flex="0 0 33.333%">
         {pictures.map((pictures) => {
-          if (pictures.id === 0 || pictures.id % 3 === 0 || pictures.id === 53 || pictures.id === 47)
+          if (pictures.id === 0 || pictures.id % 3 === 0 || pictures.id === 53)
             return <GalleryItem onclickPic={onclickPic} key={pictures.id} picture={pictures} />;
         })}
       </Box>
-      <Box display='flex' flexDirection='column'>
+      <Box display='flex' flexDirection='column' flex="0 0 33.333%">
         {pictures.map((pictures) => {
-          if ((pictures.id - 1) % 3 === 0  || pictures.id === 50)
+          if ((pictures.id - 1) % 3 === 0)
             return <GalleryItem onclickPic={onclickPic} key={pictures.id} picture={pictures} />;
         })}
       </Box>
-      <Box display='flex' flexDirection='column'>
+      <Box display='flex' flexDirection='column' flex="0 0 33.333%">
       {pictures.map((pictures ) => {
-          if ((pictures.id - 2) % 3 === 0 && pictures.id !== 53 && pictures.id !== 50 && pictures.id !== 47)
+          if ((pictures.id - 2) % 3 === 0 && pictures.id !== 53)
             return <GalleryItem onclickPic={onclickPic} key={pictures.id} picture={pictures} />;
         })}
       </Box>
